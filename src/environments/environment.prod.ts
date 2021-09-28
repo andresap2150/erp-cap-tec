b2a1a593d214f2eb31f223a3921d200d1b237946
Andres David Aparicio <andres.aparicio@globant.com>
2021-09-27 22:55:22 -0500

    se modifica el enrutamiento para ver el app desde la infra de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBnhDXS6_0iWtWSw5evpH209BMbAiRq-T8',
+    authDomain: 'awesome-apress-pwa-11a03.firebaseapp.com',
+    projectId: 'awesome-apress-pwa-11a03',
+    storageBucket: 'awesome-apress-pwa-11a03.appspot.com',
+    messagingSenderId: '220486631832',
+    appId :"1:220486631832:web:16f0f3937bdebc6b0aabed",
+    measurementId: "G-5W2ELJN5VJ"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: 'AIzaSyBnhDXS6_0iWtWSw5evpH209BMbAiRq-T8',
     authDomain: 'awesome-apress-pwa-11a03.firebaseapp.com',
     projectId: 'awesome-apress-pwa-11a03',
     storageBucket: 'awesome-apress-pwa-11a03.appspot.com',
-    messagingSenderId: '220486631832'
+    messagingSenderId: '220486631832',
+    appId :"1:220486631832:web:16f0f3937bdebc6b0aabed",
+    measurementId: "G-5W2ELJN5VJ"
   }
 };
 
