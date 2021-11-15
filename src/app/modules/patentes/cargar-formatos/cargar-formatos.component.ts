d05599e9b024eba95e239181a48c00911e9f0387
giovanni hernandez <gioherca2008@gmail.com>
2021-11-15 11:22:45 -0500

    funcionalidad cargar formatos

--- src/app/modules/patentes/cargar-formatos/cargar-formatos.component.ts
+++ src/app/modules/patentes/cargar-formatos/cargar-formatos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormatosService } from './../../core/formatos.service';
 
 @Component({
   selector: 'app-cargar-formatos',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cargar-formatos.component.scss']
 })
 export class CargarFormatosComponent implements OnInit {
+  public formatoForm : FormGroup;
+  public formatoFPath : string;
 
-  constructor() { }
+  constructor(private fs: FormatosService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formatoForm = this.fb.group({
+      nombreCol : ['',Validators.required],
+      cargoCol : ['',Validators.required],
+      tipoIdCol : ['',Validators.required],
+      idCol: ['',Validators.required],
+      filepath : ['',Validators.required],
+      clausulaProte : ['',Validators.required],
+      vencimiento : ['', Validators.required]
+    })
   }
 
+  upload(event) {    
+    this.formatoFPath = event.target.files[0]
+  }
+
+  uploadFormato(){
+    this.fs.uploadFormato(this.formatoFPath);
+  }
+
+  saveFormatoOnDb(){
+    this.uploadFormato();
+    this.checkFormValidity(this.fs.addFormato(this.formatoForm.value));
+    this.formatoForm.reset();
+  }
+
+  private checkFormValidity(cb) {
+    if (this.formatoForm.valid) {
+      cb();
+    } else {
+      console.log("debe ingresar todos los datos requeridos")
+    }
+  }
 }
